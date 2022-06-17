219d5cf18432df9b1b1440dc0f1093f90b48d29b
Mayur <m4gaikwad@gmail.com>
2022-06-17 14:52:27 +0530

    Added Comments

--- main.py
+++ main.py
@@ -2,46 +2,53 @@
 import streamlit as st
 from random import randint
 import time
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# Import CDF Parser and Pattern Generator
 from cdf_ops.cdf_parser import CdfToDf
 from cdf_ops.pattern_generator import PatternFinder
 
+#Global Variables for CDF Parser and Pattern Generator
 parser = CdfToDf()
 pattern = PatternFinder()
 MAX_FILES = 5
 
 
 def home():
-    state = st.session_state
+
+    state = st.session_state # Create a local session for Pattern Generation
+
     st.title('Philips Log Analysis')
 
+    # Specific Unique Key for Uploaded File / Files
     if 'FILE_UPLOADER_KEY' not in state:
         state.FILE_UPLOADER_KEY = str(randint(1000, 9999))
 
     #st.markdown('## \U0001F4C2 Upload CDF files')
     st.markdown('## \U0001F5CE Upload CDF Files')
 
+    # Create a list for holding 5 files
     data = st.file_uploader('', accept_multiple_files=True, type='cdf', key=state.FILE_UPLOADER_KEY)
 
+    # Check if only 5 files are uploaded
     if len(data) > 5:
         st.error('Only Five Files Can be Uploaded At A Time. Please Wait 5 Seconds To Reupload Files.')
-        time.sleep(5)
-        state.FILE_UPLOADER_KEY = str(randint(1000, 9999))
-        st.experimental_rerun()
+        time.sleep(5) # Display Message for 5 seconds
+        state.FILE_UPLOADER_KEY = str(randint(1000, 9999)) # Reinitialize Uploaded File Unique Key
+        st.experimental_rerun() # Restart Execution
 
+    # Select Errors One at a Time
     error = st.sidebar.selectbox('Select Errors', ['X_ray Generator', 'Hot Tank Oil'])
     submit = st.button('Submit')
     #print(data)
 
     if submit:
-        if len(data) != 0:
-            df = parser.convert_all(data)
-            patterns = pattern.find_patterns(df, error=error)
+        if len(data) != 0: # Check if data is not null
+            df = parser.convert_all(data) # Call cdf parser and return dataframe
+            patterns = pattern.find_patterns(df, error=error)  # Call pattern generator and return pattern
             if patterns is None:
                 st.info('Selected Patterns Found.')
             else:
-                st.dataframe(patterns)
+                st.dataframe(patterns) # Display Patterns
         else:
             st.error('Please Upload Required Files.')
 
